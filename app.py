ed85aed6335d7c1ce3389729048cd8e10163315f
GAtlas-25 <g.ghedini@atlasconcordeusa.com>
2026-02-04 15:29:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import io
 # -----------------------------------------------------------
 # Load LTL Qty file (pre-loaded in your system)
 # -----------------------------------------------------------
-LTL_QTY_PATH = "LTL_qty.xlsx"
+LTL_QTY_PATH = "LTL_qty_updated.xlsx"
 
 @st.cache_data
 def load_ltl_qty():
